f3c77941b929ee8eddaf081538708181dbbcd684
pietrofesar <pietrofesar@users.noreply.github.com>
2021-05-20 13:01:29 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-# My 2 partners and I worked together piece by piece throughout
-
 import random 
-import time
-
-R='\033[0;31m'    # red
-BR='\033[1;31m'   # bold red
-G='\033[0;32m'    # green
-BG='\033[1;32m'   # bold green
-Y='\033[0;33m'    # yellow
-BY='\033[1;33m'   # bold yellow
-B='\033[0;34m'    # blue
-BB='\033[1;34m'   # bold blue
-P='\033[0;35m'    # purple
-BP='\033[1;35m'   # bold purple
-A='\033[0;36m'    # aqua
-BA='\033[1;36m'   # bold aqua
-X='\033[0m'       # reset
 
 
 def askQuestion(question):
+    
     print(question.pop(0))
     answerKey = question[0]
     random.shuffle(question)
@@ -34,6 +18,8 @@ def askQuestion(question):
         return False
             
             
+    
+            
 def main():
     
     f = open('questions.csv', 'r')
@@ -43,16 +29,14 @@ def main():
     
     total = len(questions)
     correct = 0
-    # 3bii this is where the list is utilized
     for each in range(len(questions)):
         if askQuestion(questions.pop(0).split(',')):
             correct += 1
-        print(questions)
     percentCorrect = correct/total * 100
     
     print(f'You got {percentCorrect:.2f}% of the questions correct.')
     
-    playAgain = input(f'{R}Do you want to play again? (y / n) {X}')
+    playAgain = input(f'Do you want to play again? (y / n)')
     if playAgain == 'y':
         main()
     else:
